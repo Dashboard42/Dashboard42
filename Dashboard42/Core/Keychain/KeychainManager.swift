c25d0ed453e7ea6391a524d8fd26a03c4d45f04d
Marc MOSCA <marc.mosca@icloud.com>
2024-05-06 11:53:15 +0200

    feat: add keychain manager

---
+++ Dashboard42/Core/Keychain/KeychainManager.swift
@@ -0,0 +1,83 @@
+//
+//  KeychainManager.swift
+//  Dashboard42
+//
+//  Created by Marc MOSCA on 06/05/2024.
+//
+
+import Foundation
+
+/// An object that coordinates keychain-related tasks.
+final class KeychainManager: Keychain {
+
+    // MARK: - Properties
+
+    /// The shared singleton keychain object.
+    static let shared = KeychainManager()
+
+    private let service = "DASHBOARD42_API_KEYCHAIN_SERVICE"
+
+    // MARK: - Initializers
+
+    private init() {}
+
+    // MARK: - Errors
+
+    enum Errors: Error {
+        case unknown(OSStatus)
+    }
+
+    // MARK: - Methods
+
+    func save(account: KeychainAccount, data: String) throws {
+        let encodedData = data.data(using: .utf8) ?? .init()
+        let query = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrService as String: service as AnyObject,
+            kSecAttrAccount as String: account.rawValue as AnyObject,
+            kSecValueData as String: encodedData as AnyObject,
+        ]
+        let status = SecItemAdd(query as CFDictionary, nil)
+
+        guard status != errSecDuplicateItem else {
+            try delete(account: account)
+            return try save(account: account, data: data)
+        }
+
+        guard status == errSecSuccess else { throw Errors.unknown(status) }
+    }
+
+    func get(account: KeychainAccount) -> String? {
+        let query: [String: AnyObject] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrService as String: service as AnyObject,
+            kSecAttrAccount as String: account.rawValue as AnyObject,
+            kSecReturnData as String: kCFBooleanTrue,
+            kSecMatchLimit as String: kSecMatchLimitOne,
+        ]
+        var result: AnyObject?
+        let _ = SecItemCopyMatching(query as CFDictionary, &result)
+
+        guard let data = result as? Data else { return nil }
+
+        return String(decoding: data, as: UTF8.self)
+    }
+
+    func delete(account: KeychainAccount) throws {
+        let query = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrService as String: service as AnyObject,
+            kSecAttrAccount as String: account.rawValue as AnyObject,
+        ]
+        let status = SecItemDelete(query as CFDictionary)
+
+        guard status == errSecSuccess || status == errSecItemNotFound else { throw Errors.unknown(status) }
+    }
+
+    func clear() {
+        try? delete(account: .applicationAccessToken)
+        try? delete(account: .userAccessToken)
+        try? delete(account: .userRefreshToken)
+    }
+
+}
