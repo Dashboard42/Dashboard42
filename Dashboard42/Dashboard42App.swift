74f0810d6741d84dc2313e75d6c898db4dd0c43f
Marc MOSCA <marc.mosca@icloud.com>
2024-05-06 10:48:28 +0200

    Initial Commit

---
+++ Dashboard42/Dashboard42App.swift
@@ -0,0 +1,17 @@
+//
+//  Dashboard42App.swift
+//  Dashboard42
+//
+//  Created by Marc MOSCA on 06/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct Dashboard42App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
