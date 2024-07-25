cae90baea82395cb780a870fcf0ffb0d672e8c1c
Marc MOSCA <marc.mosca@icloud.com>
2024-07-25 18:46:37 +0200

    refactor: remove pre-defined tests

--- Dashboard42/Views/Components/Atoms/AsyncButton.swift
+++ Dashboard42/Views/Components/Atoms/AsyncButton.swift
@@ -7,12 +7,52 @@
 
 import SwiftUI
 
-struct AsyncButton: View {
+struct AsyncButton<Label>: View where Label: View {
+    let action: () async -> Void
+    let label: () -> Label
+
+    @State private var isPerformingTask: Bool = false
+
     var body: some View {
-        Text("Hello, World!")
+        Button(
+            action: {
+                isPerformingTask = true
+
+                Task {
+                    await action()
+                    isPerformingTask = false
+                }
+            },
+            label: {
+                ZStack {
+                    label().opacity(isPerformingTask ? 0 : 1)
+
+                    if isPerformingTask {
+                        ProgressView()
+                    }
+                }
+            }
+        )
+        .disabled(isPerformingTask)
+    }
+}
+
+extension AsyncButton where Label == Text {
+    init(_ label: LocalizedStringKey, action: @escaping () async -> Void) {
+        self.init(action: action) {
+            Text(label)
+        }
+    }
+}
+
+extension AsyncButton where Label == Image {
+    init(systemImageName: String, action: @escaping () async -> Void) {
+        self.init(action: action) {
+            Image(systemName: systemImageName)
+        }
     }
 }
 
 #Preview {
-    AsyncButton()
+    AsyncButton("Hello, World", action: { try? await Task.sleep(for: .seconds(2)) })
 }
