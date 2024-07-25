7ba404ffbed1012591a306caba9953c4fcc1b6c0
Marc MOSCA <marc.mosca@icloud.com>
2024-07-25 17:17:02 +0200

    refactor: add unit and ui tests

---
+++ Dashboard42UITests/Dashboard42UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Dashboard42UITestsLaunchTests.swift
+//  Dashboard42UITests
+//
+//  Created by Marc MOSCA on 25/07/2024.
+//
+
+import XCTest
+
+final class Dashboard42UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
