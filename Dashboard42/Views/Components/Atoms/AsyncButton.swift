5494719981eea7d67b095423b3991cb952b85f99
Marc MOSCA <marc.mosca@icloud.com>
2024-07-25 18:13:03 +0200

    feat: add swift format run script to build phase

---
+++ Dashboard42/Views/Components/Atoms/AsyncButton.swift
@@ -0,0 +1,18 @@
+//
+//  AsyncButton.swift
+//  Dashboard42
+//
+//  Created by Marc MOSCA on 25/07/2024.
+//
+
+import SwiftUI
+
+struct AsyncButton: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    AsyncButton()
+}
