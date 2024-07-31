a5283a842bcc74a3bbf9b5aeff25397f1936c286
Marc MOSCA <marc.mosca@icloud.com>
2024-07-31 18:57:22 +0200

    docs: add comments to async button component

--- Dashboard42/Views/Components/Atoms/AsyncButton.swift
+++ Dashboard42/Views/Components/Atoms/AsyncButton.swift
@@ -7,49 +7,73 @@
 
 import SwiftUI
 
+/// A control that initiates an asynchronous action.
 struct AsyncButton<Label>: View where Label: View {
     let action: () async -> Void
     let label: () -> Label
+
+    /// Creates a button that displays a custom label.
+    ///
+    /// - Parameters:
+    ///   - action: The asynchronous action to perform when the user triggers the button.
+    ///   - label: A view that describes the purpose of the button's `action`.
+    init(action: @escaping () async -> Void, label: @escaping () -> Label) {
+        self.action = action
+        self.label = label
+    }
 
     @State private var isPerformingTask: Bool = false
 
     var body: some View {
         Button(
             action: {
-                isPerformingTask = true
+                self.isPerformingTask = true
 
                 Task {
-                    await action()
-                    isPerformingTask = false
+                    await self.action()
+                    self.isPerformingTask = false
                 }
             },
             label: {
                 ZStack {
-                    label()
-                        .opacity(isPerformingTask ? 0 : 1)
+                    self.label()
+                        .opacity(self.isPerformingTask ? 0 : 1)
 
-                    if isPerformingTask {
+                    if self.isPerformingTask {
                         ProgressView()
                     }
                 }
             }
         )
-        .disabled(isPerformingTask)
+        .disabled(self.isPerformingTask)
     }
 }
 
 extension AsyncButton where Label == Text {
-    init(_ label: LocalizedStringKey, action: @escaping () async -> Void) {
+
+    /// Creates a button that generates its label from a localized string key.
+    ///
+    /// - Parameters:
+    ///   - titleKey: The key for the button's localized title, that describes the purpose of the button's `action`.
+    ///   - action: The action to perform when the user triggers the button.
+    init(_ titleKey: LocalizedStringKey, action: @escaping () async -> Void) {
         self.init(action: action) {
-            Text(label)
+            Text(titleKey)
         }
     }
+
 }
 
 extension AsyncButton where Label == Image {
-    init(systemImageName: String, action: @escaping () async -> Void) {
+
+    /// Creates a button that generates its label from a system image name.
+    ///
+    /// - Parameters:
+    ///   - systemImage: The name of the image resource to lookup.
+    ///   - action: The action to perform when the user triggers the button.
+    init(systemImage: String, action: @escaping () async -> Void) {
         self.init(action: action) {
-            Image(systemName: systemImageName)
+            Image(systemName: systemImage)
         }
     }
 }
