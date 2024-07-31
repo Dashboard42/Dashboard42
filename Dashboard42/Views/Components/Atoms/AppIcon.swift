e4d44d2704f96174f7fa28dadb3d5adf56e546ee
Marc MOSCA <marc.mosca@icloud.com>
2024-07-31 19:12:38 +0200

    feat: add app icon view component

---
+++ Dashboard42/Views/Components/Atoms/AppIcon.swift
@@ -0,0 +1,34 @@
+//
+//  AppIcon.swift
+//  Dashboard42
+//
+//  Created by Marc MOSCA on 31/07/2024.
+//
+
+import SwiftUI
+
+/// A view that displays the application icon.
+struct AppIcon: View {
+
+    /// Creates the application icon image.
+    init() {}
+
+    var appIcon: String {
+        guard let icons = Bundle.main.object(forInfoDictionaryKey: "CFBundleIcons") as? [String: Any],
+            let primaryIcon = icons["CFBundlePrimaryIcon"] as? [String: Any],
+            let iconFiles = primaryIcon["CFBundleIconFiles"] as? [String],
+            let iconFileName = iconFiles.last
+        else {
+            fatalError("Could not find icons in bundle")
+        }
+        return iconFileName
+    }
+
+    var body: some View {
+        Image(uiImage: UIImage(imageLiteralResourceName: appIcon))
+    }
+}
+
+#Preview {
+    AppIcon()
+}
